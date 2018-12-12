c4c3d2684064ee770d64dc55a2736ad8e977e063
liyh <liyinghui1987>
2018-12-12 18:06:35 +0800

    修改

--- app/src/main/java/com/icbc/rxjava2demos/generate/CreateActivity.java
+++ app/src/main/java/com/icbc/rxjava2demos/generate/CreateActivity.java
@@ -2,14 +2,42 @@ package com.icbc.rxjava2demos.generate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.icbc.rxjava2demos.R;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+
 public class CreateActivity extends AppCompatActivity {
+    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        create = findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Disposable disposable = Observable.create(new ObservableOnSubscribe() {
+                    @Override
+                    public void subscribe(ObservableEmitter e) throws Exception {
+
+                    }
+                }).subscribe(new Consumer() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+
+                    }
+                });
+
+            }
+        });
     }
 }
